c3cdbc36ebe7215d654d954edc47f35b3499f1e9
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-13 15:20:12 +0530

    signin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
 const{blogmodel}=require("./model/blog")
 mongoose.connect("mongodb+srv://sreya:sreya123@cluster0.rk6cqoj.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
 const app=express()
@@ -23,6 +24,36 @@ app.post("/signup",async(req,res)=>{
     blog.save()
     console.log(blog)
     res.json({"status":"success"})
+})
+
+app.post("/signin",(req,res)=>{
+   let input=req.body
+   blogmodel.find({"email":req.body.email}).then(
+    (response)=>{
+        if (response.length>0) {
+            let dbPassword=response[0].password
+            console.log(dbPassword)
+            bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                if (isMatch) {
+                   jwt.sign({"email":input.email},"blog-app",{expiresIn:"1d"},
+                   (error,token)=>{
+                    if (error) {
+                        res.json({"status":"unable to create token"})
+                    } else {
+                        res.json({"status":"success","userid":response[0]._id,"token":token})
+                    }
+                   }
+                )
+                } else {
+                    res.json({"status":"incorrect"})
+                }
+            })
+            
+        } else {
+            res.json({"status":"user not found"})
+        }
+        }
+   ).catch()
 })
 
 app.listen(8080,()=>{
